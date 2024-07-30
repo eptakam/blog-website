0d934e75658b9e77e7f493449a5298c8df9c450d
eptakam <emmataks@gmail.com>
2024-07-30 14:29:34 -0300

    Adding styling and logo.

---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import classes from '../../styles/logo.module.css';
+
+export default function Logo() {
+  return (
+    <div className={classes.logo}>
+      Emmanuel Next Blog
+    </div>
+  );
+}
